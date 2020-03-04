958b2ad58b521d68944c58497e46c4c97d3096e4
Nacho <iortegamancera@gmail.com>
2020-03-04 16:45:40 +0100

    [ADD] Added custom navbar component and needed styled-components

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,7 +1,46 @@
+import styled from 'styled-components';
+import AppBar from '@material-ui/core/AppBar';
 import React from 'react';
+import { Container } from '@material-ui/core';
+import {
+  ToolbarWrapper,
+  LogoWrapper,
+  ImgWrapper,
+  MenuItemsWrapper,
+  ButtonWrapper,
+} from './styled-components';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+export const Navbar = () => {
+  return (
+    <NavbarWrapper className="rick-primary">
+      <Container>
+        <ToolbarWrapper>
+          <LogoWrapper>
+            <ImgWrapper
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg"
+              alt="Reacty & Morty"
+            />
+          </LogoWrapper>
+          <MenuItemsWrapper>
+            <ButtonWrapper className="white" color="secondary">
+              Inicio
+            </ButtonWrapper>
+            <ButtonWrapper className="white" color="secondary">
+              Capítulos
+            </ButtonWrapper>
+            <ButtonWrapper className="white" color="secondary">
+              Planetas
+            </ButtonWrapper>
+          </MenuItemsWrapper>
+        </ToolbarWrapper>
+      </Container>
+    </NavbarWrapper>
+  );
 };
 
-export default Navbar;
+const NavbarWrapper = styled(AppBar)`
+  max-height: 100%;
+  &.rick-primary {
+    background-color: #add4e8;
+  }
+`;
