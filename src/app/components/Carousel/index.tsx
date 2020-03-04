12efc016a19f8a570cbe48bb65f8aabb4fa9c11d
Alvaro Orellana <alvaroorellana9@hotmail.com>
2020-03-04 17:24:48 +0100

    [ADD] Added necessary dependencies [MOD] Dynamiced carousel

--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -1,31 +1,74 @@
 import * as React from 'react';
+import Loader from 'react-loader-spinner';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
+import axios from 'axios';
+
+const getRandomIDs = async () => {
+  let characterIDs: number[] = [];
+  const result = await axios.get('https://rickandmortyapi.com/api/character/');
+  while (characterIDs.length < 5) {
+    let number = Math.floor(Math.random() * result.data.info.count) + 1;
+    if (!characterIDs.includes(number)) {
+      characterIDs.push(number);
+    }
+  }
+  return characterIDs;
+};
+
+// const getCharacter = (characterID: number) => {
+//   const characterInfo = axios.get(
+//     `https://rickandmortyapi.com/api/character/${characterID}`
+//   );
+//   return characterInfo;
+// };
+
+const getCharacterInfos = async (characterIDs: number[]) => {
+  let results = await Promise.all(
+    characterIDs.map(async (item, index) => {
+      const char = await axios.get(
+        `https://rickandmortyapi.com/api/character/${item}`
+      );
+      return char;
+    })
+  );
+  return results;
+};
+
+const generateCharactersData = async () => {
+  const characterIDs = await getRandomIDs();
+  const characterInfos = await getCharacterInfos(characterIDs);
+  return characterInfos;
+};
 
 const CustomCarousel = () => {
+  const [characterInfos, setcharacterInfos] = React.useState<any[]>(
+    [] as any[]
+  );
+  const [isLoadedCarousel, setisLoadedCarousel] = React.useState<boolean>(
+    false
+  );
+
+  React.useEffect(() => {
+    const fetchCarousel = async () => {
+      setcharacterInfos(await generateCharactersData());
+      setisLoadedCarousel(true);
+    };
+    fetchCarousel();
+  }, []);
+
+  if (isLoadedCarousel === false) {
+    return <Loader width={500} />;
+  }
   return (
     <Carousel width="25%" infiniteLoop>
-      <div>
-        <img
-          alt="rick"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-        />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img
-          alt="morty"
-          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-        />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img
-          alt="summer"
-          src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-        />
-        <p className="legend">Legend 3</p>
-      </div>
+      {characterInfos.length > 0 &&
+        characterInfos.map(item => (
+          <div>
+            <img src={item.data.image} alt="slide" />
+            <p className="legend">{item.data.name}</p>
+          </div>
+        ))}
     </Carousel>
   );
 };
