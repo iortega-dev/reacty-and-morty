5b840299f555c932c675aab4955ad715b70e2df2
Jorge Gutierrez <jorge.gutierrezvila0@gmail.com>
2020-03-04 12:43:02 +0100

    [ADD] Styles to image, character information and icons

--- src/app/components/Tiles/index.tsx
+++ src/app/components/Tiles/index.tsx
@@ -3,15 +3,87 @@ import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import { CardContent } from '@material-ui/core';
+import image from '~Assets/img/rickandmortyimage.jpg';
+import {
+  ImageContainer,
+  GridImage,
+  IconsContainer,
+  InformationList,
+  InformationListElement,
+  Characteristic,
+  CustomPlaceOutlinedIcon,
+  CustomMovieIcon,
+} from '~Styles/Tiles/Styles';
 
 const Tiles = () => {
   return (
     <Grid container spacing={4}>
-      <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+      <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
         <Card>
           <CardContent>
             <Grid item xs={12} sm container direction={'row'}>
-              <Grid item xs={4}>
+              <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+                <ImageContainer>
+                  <GridImage src={image} />
+                </ImageContainer>
+              </Grid>
+              <Grid
+                item
+                xs={11}
+                sm={11}
+                md={7}
+                lg={7}
+                xl={7}
+                container
+                direction={'column'}
+                spacing={2}
+              >
+                <Grid item xs>
+                  <InformationList>
+                    <InformationListElement>
+                      <Characteristic>Name: </Characteristic>
+                      Esto es una prueba con un nombre largo
+                    </InformationListElement>
+                    <InformationListElement>
+                      <Characteristic>Status: </Characteristic>
+                      Alive
+                    </InformationListElement>
+                    <InformationListElement>
+                      <Characteristic>Gender: </Characteristic>
+                      Male
+                    </InformationListElement>
+                    <InformationListElement>
+                      <Characteristic>Specie: </Characteristic>
+                      Esto es un nombre de especie algo más largo de lo esperado
+                    </InformationListElement>
+                  </InformationList>
+                </Grid>
+              </Grid>
+              <Grid xs={1} sm={1} md={1} lg={1} xl={1} item>
+                <IconsContainer>
+                  <CustomPlaceOutlinedIcon
+                    fontSize={'large'}
+                    onClick={() => {
+                      console.log('Se ha clickado en el icono de place');
+                    }}
+                  />
+                  <CustomMovieIcon
+                    fontSize={'large'}
+                    onClick={() => {
+                      console.log('Se ha clickado en el icono de movie');
+                    }}
+                  />
+                </IconsContainer>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        <Card>
+          <CardContent>
+            <Grid item xs={12} sm container direction={'row'}>
+              <Grid item xs={3}>
                 Imagen
               </Grid>
               <Grid item xs={7} container direction={'column'} spacing={2}>
@@ -30,11 +102,11 @@ const Tiles = () => {
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+      <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
         <Card>
           <CardContent>
             <Grid item xs={12} sm container direction={'row'}>
-              <Grid item xs={4}>
+              <Grid item xs={3}>
                 Imagen
               </Grid>
               <Grid item xs={7} container direction={'column'} spacing={2}>
@@ -53,30 +125,7 @@ const Tiles = () => {
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <Card>
-          <CardContent>
-            <Grid item xs={12} sm container direction={'row'}>
-              <Grid item xs={4}>
-                Imagen
-              </Grid>
-              <Grid item xs={7} container direction={'column'} spacing={2}>
-                <Grid item xs>
-                  <Typography>Name: Prueba</Typography>
-                  <Typography>Status: Prueba</Typography>
-                  <Typography>Gender: Prueba</Typography>
-                  <Typography>Specie: Prueba</Typography>
-                </Grid>
-              </Grid>
-              <Grid xs={1} item>
-                <Typography>Icono 1</Typography>
-                <Typography>Icono 2</Typography>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+      <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
         <Card>
           <CardContent>
             <Grid item xs={12} sm container direction={'row'}>
---
+++ src/app/styles/Tiles/Styles.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { PlaceOutlined, Movie } from '@material-ui/icons';
+
+export const ImageContainer = styled.div`
+  margin: auto;
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+  padding-right: 10px;
+  padding-left: 10px;
+`;
+
+export const GridImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  object-fit: fill;
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  margin-left: 15px;
+  height: 100%;
+`;
+
+export const InformationList = styled.ul`
+  padding-left: 15px;
+  margin: 0;
+`;
+
+export const InformationListElement = styled.li`
+  padding-bottom: 5px;
+`;
+
+export const Characteristic = styled.span`
+  font-weight: bold;
+`;
+
+export const CustomPlaceOutlinedIcon = styled(PlaceOutlined)`
+  color: #1f98a9;
+`;
+
+export const CustomMovieIcon = styled(Movie)`
+  color: #1f98a9;
+  margin-bottom: 10px;
+`;
