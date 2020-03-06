d7c891b51b26a115223e9b5993fc660da458a2d7
Jorge Gutierrez <jorge.gutierrezvila0@gmail.com>
2020-03-06 14:11:45 +0100

    [MOD] Little changes showing location.

--- src/app/components/Tiles/Tiles/LocationInformation.tsx
+++ src/app/components/Tiles/Tiles/LocationInformation.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { CardContent } from '@material-ui/core';
+import { CardContent, Typography } from '@material-ui/core';
 import { CharacterLocation } from '~Types/Character';
+
+// TODO -> links to location using route
 
 const LocationInformation = (props: { location: CharacterLocation }) => {
   const { location } = props;
   return (
     <CardContent>
+      <Typography gutterBottom variant={'h5'} component={'h2'}>
+        Location
+      </Typography>
+      <hr/>
       <p>
-        {location.name},{location.url}
+        {location.name} -> {location.url}
       </p>
     </CardContent>
   );
