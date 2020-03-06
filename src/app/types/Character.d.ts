2609b28b96f93b6c9c98fc57d63c971841299d29
Nacho <iortegamancera@gmail.com>
2020-03-06 10:08:50 +0100

    [ADD] Defined application entity interfaces

---
+++ src/app/types/Character.d.ts
@@ -0,0 +1,16 @@
+type Gender = 'Female' | 'Male' | 'Genderless' | 'unkwnown';
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: Gender;
+  origin: object;
+  location: object;
+  image: string;
+  episode: array<string>;
+  url: string;
+  created: string;
+}
