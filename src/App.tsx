22dfe6fa3f20ec03336c7013b5ee81753a16d31d
Alvaro Orellana <alvaroorellana9@hotmail.com>
2020-03-06 12:11:39 +0100

    [MOD] Refactor [MOD] Modified App.tsx to fit Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 // Local Imports
 import { Navbar } from '~Components/Navbar';
 import { Typography } from '~Assets/fonts';
+import CustomCarousel from '~Components/Carousel';
 
 const App = () => {
   return (
     <>
       <Typography />
       <Navbar />
+      <CustomCarousel />
     </>
   );
 };
--- src/app/components/Carousel/index.tsx
+++ src/app/components/Carousel/index.tsx
@@ -2,42 +2,7 @@ import * as React from 'react';
 import Loader from 'react-loader-spinner';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
-import axios from 'axios';
-
-const getRandomIDs = async (limit: number) => {
-  let characterIDs: number[] = [];
-  const result = await axios.get('https://rickandmortyapi.com/api/character/');
-  while (characterIDs.length < limit) {
-    let number = Math.floor(Math.random() * result.data.info.count) + 1;
-    if (!characterIDs.includes(number)) {
-      characterIDs.push(number);
-    }
-  }
-  return characterIDs;
-};
-
-const getCharacter = (characterID: number) => {
-  const characterInfo = axios.get(
-    `https://rickandmortyapi.com/api/character/${characterID}`
-  );
-  return characterInfo;
-};
-
-const getCharacterInfos = async (characterIDs: number[]) => {
-  let results = await Promise.all(
-    characterIDs.map(async (item, index) => {
-      const char = getCharacter(item);
-      return char;
-    })
-  );
-  return results;
-};
-
-const generateCharactersData = async () => {
-  const characterIDs = await getRandomIDs(10);
-  const characterInfos = await getCharacterInfos(characterIDs);
-  return characterInfos;
-};
+import { generateCharactersData } from '~Utils/index';
 
 const CustomCarousel = () => {
   const [characterInfos, setcharacterInfos] = React.useState<any[]>(
@@ -55,7 +20,7 @@ const CustomCarousel = () => {
     fetchCarousel();
   }, []);
 
-  if (isLoadedCarousel === false) {
+  if (!isLoadedCarousel) {
     return <Loader type="Oval" width={500} />;
   }
   return (
