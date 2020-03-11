8395024a3d13c1587b1d03f1ec4f09654f690723
Nacho <ignacio.ortega@opinno.com>
2020-03-11 11:53:55 +0100

    [ADD] Added Futurama alien font

--- src/app/assets/fonts/index.js
+++ src/app/assets/fonts/index.js
@@ -4,11 +4,22 @@ import CalligraphrTTF from './Calligraphr-Regular.ttf';
 import CalligraphrWOFF from './Calligraphr-Regular.woff';
 import CalligraphrWOFF2 from './Calligraphr-Regular.woff2';
 
+import FuturamaTTF from './fr-fal1.ttf';
+import FuturamaWOFF from './fr-fal1.woff';
+
 export const Typography = createGlobalStyle`
     @font-face {
         font-family: 'Calligraphr';
         src: local('Calligraphr'), local('Calligraphr Regular'), url(${CalligraphrTTF}) format('truetype'),
         url(${CalligraphrWOFF}) format('woff2'),
         url(${CalligraphrWOFF2}) format('woff');
+    }
+
+    @font-face {
+        font-family:"Futurama Alien";
+        src: url(${FuturamaWOFF}) format("woff"),
+        url(${FuturamaTTF}) format("truetype");
+        font-weight:normal;
+        font-style:normal;
     }
 `;
