ed854e102b62945eda7081df301ff3959efa8621
iortega-dev <iortegamancera@gmail.com>
2020-03-12 16:50:30 +0100

    [ADD] First step - useEffect

--- src/app/components/StateHooksComponent/index.tsx
+++ src/app/components/StateHooksComponent/index.tsx
@@ -1,7 +1,23 @@
-import React, { useState, FormEvent, ChangeEvent } from 'react';
+import React, { useState, FormEvent, ChangeEvent, useEffect } from 'react';
 
 const StateHooksComponent = () => {
   const [name, setName] = useState<string>('');
+  const [address, setAddress] = useState<string>('');
+
+  useEffect(() => {
+    console.log('Component mounted');
+    return () => {
+      console.log('Component will be unmount');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`Any state changed Name: ${name}, Address: ${address}`);
+  });
+
+  useEffect(() => {
+    console.log(`Name changed: ${name}`);
+  }, [name]);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -10,6 +26,10 @@ const StateHooksComponent = () => {
 
   const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
+  };
+
+  const onAddressChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
   };
 
   return (
@@ -21,6 +41,12 @@ const StateHooksComponent = () => {
           placeholder="name"
           value={name}
           onChange={onNameChange}
+        />
+        <input
+          type="text"
+          placeholder="address"
+          value={address}
+          onChange={onAddressChange}
         />
         <button type="submit">Submit</button>
       </form>
