a81b2efd35308ad97af1a59fd658a099af116372
iortega-dev <iortegamancera@gmail.com>
2020-03-12 17:32:38 +0100

    [MOD] Updated rootReducer references

--- src/app/store/index.d.ts
+++ src/app/store/index.d.ts
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { InitialState, DispatchAction, rootReducer } from './root-reducer.ts'; // check with Alexis
+import { InitialState, DispatchAction, rootReducer } from './root-reducer.d.ts'; // check with Alexis
 
 export const store = createStore<InitialState, DispatchAction, null, null>(
   rootReducer
