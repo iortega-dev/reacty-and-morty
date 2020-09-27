9081739ae84e59af8d31a55c69805e0cd0cfc678
iortega-dev <50416709+iortega-dev@users.noreply.github.com>
2020-09-27 18:18:38 +0000

    Deploying to gh-pages from  @ 9abb3af9286fbf9645a3a22555bfc8ded733d853 🚀

---
+++ precache-manifest.adcf0a682bb8a624a10067bfee09564b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697b1577e2ffbbb57ef2620775197c04",
+    "url": "/reacty-and-morty/index.html"
+  },
+  {
+    "revision": "c19a4218c8bfee60841b",
+    "url": "/reacty-and-morty/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "c6ed4cf34dd1f73d974d",
+    "url": "/reacty-and-morty/static/css/main.a619c6bc.chunk.css"
+  },
+  {
+    "revision": "c19a4218c8bfee60841b",
+    "url": "/reacty-and-morty/static/js/2.221e27e7.chunk.js"
+  },
+  {
+    "revision": "fd03bfd92170a7e5732eca751272ef6d",
+    "url": "/reacty-and-morty/static/js/2.221e27e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ed4cf34dd1f73d974d",
+    "url": "/reacty-and-morty/static/js/main.992274ca.chunk.js"
+  },
+  {
+    "revision": "86b6324a29eafb50aaed",
+    "url": "/reacty-and-morty/static/js/runtime-main.246ec1c2.js"
+  },
+  {
+    "revision": "1801a3694b1faff4a369a310bc698cc1",
+    "url": "/reacty-and-morty/static/media/Calligraphr-Regular.1801a369.ttf"
+  },
+  {
+    "revision": "50adb25abd71899e04f4ec8bb59c0df1",
+    "url": "/reacty-and-morty/static/media/Calligraphr-Regular.50adb25a.woff"
+  },
+  {
+    "revision": "f8fa34e3f41f0fcd85ee83e601eeefad",
+    "url": "/reacty-and-morty/static/media/Calligraphr-Regular.f8fa34e3.woff2"
+  },
+  {
+    "revision": "08bd957a4da9c3ed0ffecd383f07deb1",
+    "url": "/reacty-and-morty/static/media/fr-fal1.08bd957a.ttf"
+  },
+  {
+    "revision": "472528fb4c80ca51423f92a8a292f880",
+    "url": "/reacty-and-morty/static/media/fr-fal1.472528fb.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f79d213134d04395851b74549bff1b9.js"
+  "/reacty-and-morty/precache-manifest.adcf0a682bb8a624a10067bfee09564b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reacty-and-morty/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
